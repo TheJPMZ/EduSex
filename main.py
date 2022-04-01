7e78248e6e9ddf3dbf54784ab163580bddc3b2d3
TheJPMZ <monzon.jpmz@gmail.com>
2022-04-01 09:00:57 -0600

    ➕ Created first testing views

--- main.py
+++ main.py
@@ -1,13 +1,19 @@
 from kivy.app import App
+from kivy.lang import Builder
+from kivy.uix.screenmanager import ScreenManager
 from kivy.uix.widget import Widget
 
-
-class MainWidget(Widget):
-    pass
+import menu
+import settings
 
 
 class EduSexApp(App):
-    pass
+    def build(self):
+        sm = ScreenManager()
+        sm.add_widget(menu.MainMenu(name="menu"))
+        sm.add_widget(settings.Settings(name="settings"))
+        return sm
 
 
-EduSexApp().run()
+if __name__ == '__main__':
+    EduSexApp().run()
---
+++ settings.py
@@ -0,0 +1,8 @@
+from kivy.lang import Builder
+from kivy.uix.screenmanager import Screen
+
+Builder.load_file('settings.kv')
+
+
+class Settings(Screen):
+    pass
