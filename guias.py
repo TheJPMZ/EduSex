3da99defe8f04728f5a87895ffc7a2f01a247adb
TheJPMZ <monzon.jpmz@gmail.com>
2022-04-01 16:06:18 -0600

    ➕ Added guias.kv

---
+++ guias.py
@@ -0,0 +1,43 @@
+from kivy.lang import Builder
+from kivy.uix.screenmanager import Screen
+from kivy.uix.button import Button
+from kivy.uix.boxlayout import BoxLayout
+from kivy.uix.stacklayout import StackLayout
+
+
+Builder.load_file('guias.kv')
+
+
+dicGuias = {
+    "Guia1" : 25,
+    "Guia2" : 25,
+    "Guia3" : 25,
+    "Guia4" : 100,
+    "Guia5" : 100,
+    "Guia6" : 100,
+}
+
+class RoundedButton(Button):
+    pass
+
+class Tile(RoundedButton):
+    pass
+
+class InnerStack(StackLayout):
+
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+        for x, y in dicGuias.items():
+            b = Tile(text=x,
+                     size_hint=(.2, .2)
+                     )
+            self.add_widget(b)
+
+
+class Guias(Screen):
+
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+
+
+    pass
--- main.py
+++ main.py
@@ -7,6 +7,7 @@ from kivy.core.window import Window
 import menu
 import settings
 import genero
+import guias
 
 
 Window.clearcolor = (1, 1, 1, 1)
@@ -17,6 +18,7 @@ class EduSexApp(App):
         sm.add_widget(genero.Genero(name="genero"))
         sm.add_widget(menu.MainMenu(name="menu"))
         sm.add_widget(settings.Settings(name="settings"))
+        sm.add_widget(guias.Guias(name="guias"))
         return sm
 
 
