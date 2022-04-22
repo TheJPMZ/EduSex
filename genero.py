1f0a5a817aaf5822ad5d661b5816c8d3b4ed16c4
TheJPMZ <monzon.jpmz@gmail.com>
2022-04-22 08:15:08 -0600

    ➕Genero se guarda globalmente

--- genero.py
+++ genero.py
@@ -3,12 +3,17 @@ from kivy.uix.screenmanager import Screen
 
 Builder.load_file('genero.kv')
 
+def store_data(data):
+    with open('data.txt', 'w') as f:
+        f.write(data)
+
+
 
 class Genero(Screen):
     genero = ""
     def save_gender(self, gender):
         self.genero = gender
-        print(self.genero)
+        store_data(self.genero)
         self.manager.current = "menu"
 
     pass
--- main.py
+++ main.py
@@ -9,19 +9,33 @@ import menu
 import settings
 import genero
 import guias
-
+import preservativos
+import pguias
 
 Window.clearcolor = (1, 1, 1, 1)
 Window.size = (1080/3,1920/3)
 
+#Check if there is a file named data.txt with a string, return the string
+def get_data():
+    try:
+        with open('data.txt', 'r') as f:
+            if f.read() != '':
+                return True
+            else:
+                return False
+    except FileNotFoundError:
+        return False
+
 class EduSexApp(App):
     def build(self):
         sm = ScreenManager()
-        sm.add_widget(genero.Genero(name="genero"))
+        if not get_data():
+            sm.add_widget(genero.Genero(name="genero"))
         sm.add_widget(menu.MainMenu(name="menu"))
         sm.add_widget(settings.Settings(name="settings"))
         sm.add_widget(guias.Guias(name="guias"))
-
+        sm.add_widget(preservativos.Preservativos(name="preservativos"))
+        # sm.add_widget(pguias.Plantilla(name ="plantilla"))
         return sm
 
 
