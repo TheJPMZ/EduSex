4c0e9941169c144fa5fd96ec63c12936a4899504
Alien Ware <jacmg01@gmail.com>
2022-04-22 10:38:46 -0600

    se pueden gastar puntos

--- puntos.py
+++ puntos.py
@@ -1,7 +1,7 @@
 import json
 import random
 
-def ganar(cantidad_buenas):
+def ganar(cantidad_buenas,nombre_guia):
     with open('usuario.json', 'r+') as f:
         data = json.load(f)
         
@@ -11,7 +11,12 @@ def ganar(cantidad_buenas):
         else:
             boost = random.randint(2,5)
         puntos_ganados = int(adicion*boost)
-        data['puntos'] += puntos_ganados
+        if nombre_guia not in data["Guias terminadas"]:
+            data['puntos'] += puntos_ganados
+            data["Guias terminadas"].append(nombre_guia)
+        else:
+            print("Ya has ganado puntos en esta guia")
+            
         f.seek(0)  # rewind
         json.dump(data, f)
         f.truncate()
@@ -21,7 +26,16 @@ def ganar(cantidad_buenas):
 
         
 def gastar(puntos_a_gastar):
-    pass
+    with open('usuario.json', 'r+') as f:
+        data = json.load(f)
+        if data['puntos'] < puntos_a_gastar:
+           print("No tienes suficientes puntos")
+        else:
+            data['puntos'] -= puntos_a_gastar
+            f.seek(0)  # rewind
+            json.dump(data, f)
+            f.truncate()
+            f.close()
     
     
     
