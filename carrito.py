7aa21ef69785ee0fedfc74f6d396187d60d8098a
Priscigs <39099667+Priscigs@users.noreply.github.com>
2022-05-13 14:10:19 -0600

    🚘 Cart Changes 2

--- carrito.py
+++ carrito.py
@@ -1,4 +1,5 @@
 from cgitb import text
+from curses import color_content
 from pydoc import describe
 from tkinter import Variable
 from turtle import heading
@@ -7,59 +8,64 @@ from typing import Tuple
 from kivy.lang import Builder
 from kivy.uix.screenmanager import Screen
 from kivy.uix.button import Button
-from kivy.uix.boxlayout import BoxLayout
-from kivy.uix.stacklayout import StackLayout
 from kivy.uix.label import Label
-from kivy.uix.scrollview import ScrollView
-from kivy.uix.image import Image
-from kivy.uix.recycleview import RecycleView
-from numpy import size
-from kivy.core.window import Window
-from math import ceil
-from kivy.uix.checkbox import CheckBox
 import json
+import anillo
+import capuchon
 
 Builder.load_file('carrito.kv')
 
-productos = {}
+numero = 0
 
 with open('productos.json', 'r', encoding="utf_8") as f:
     productos = json.load(f)
+    
 
 class RoundedButton(Button):
     pass
 
-global numero
-
 class Carrito(Screen):
 
-    def set_list(self, text="", search=False):
-        def add_item(name):
+    def set_list(self):
+        def addToCart(name):
             self.ids.product.add_widget(
-                Button(
+                Label(
                     size_hint=(1, None),
                     size_y=100,
                     text=name,
                     text_size=(300, None),
-                    background_normal = "",
-                    background_color= (.97, .79, .89),
                     color = (0,0,0)
-
                 )
             )
-
         self.ids.product.clear_widgets()
-        for pregunta in productos.keys():
-            if search:
-                if text in pregunta:
-                    add_item(pregunta)
-                    continue
-                for y in productos[pregunta].get("tags"):
-                    if text in y:
-                        add_item(pregunta)
-                        continue
-            else:
-                add_item(pregunta)
+
+        addToCart(productos["Anillo Vaginal"])
+        addToCart(productos["Precio Anillo Vaginal"])
+
+    def agregar (self):
+        while numero < 5:
+            self.ids.contador.add_widget(
+                Label(
+                    size_hint=(1, None),
+                    size_y=100,
+                    text_size=(300, None),
+                    color = (0,0,0)
+                )
+            )
+        numero += 1
+    
+    def quitar (self):
+        while self.numero < 5:
+            self.ids.contador.add_widget(
+                Label(
+                    size_hint=(1, None),
+                    size_y=100,
+                    text_size=(300, None),
+                    color = (0,0,0)
+                )
+            )
+            numero -= 1
+     
 
     def __init__(self, **kw):
         super().__init__(**kw)
