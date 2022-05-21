1bb00bdfcd3044b4c0eae16bf3b55d22f4a12906
Priscigs <fatimag811@gmail.com>
2022-05-20 23:19:42 -0600

    🤔 Changes in Section "Dudas"

--- dudas.py
+++ dudas.py
@@ -1,7 +1,4 @@
 from kivy.properties import ListProperty
-from kivymd.app import MDApp
-from kivymd.uix.list import OneLineAvatarIconListItem
-from kivymd.uix.textfield import MDTextField
 from kivy.lang import Builder
 from kivy.uix.screenmanager import Screen
 from kivy.uix.button import Button
@@ -9,12 +6,24 @@ from kivy.uix.boxlayout import BoxLayout
 from kivy.uix.stacklayout import StackLayout
 from kivy.uix.label import Label
 from kivy.uix.scrollview import ScrollView
-from kivy.uix.image import Image
-from kivy.uix.recycleview import RecycleView
-from kivymd.uix.expansionpanel import MDExpansionPanel, MDExpansionPanelLabel
-from kivymd.uix.boxlayout import MDBoxLayout
+from math import ceil
+from kivymd.uix.button import MDIconButton
 
 import json
+
+from numpy import size
+
+dicGuias = {
+    "Guia6": 100,
+    "Guia7": 100,
+    "Guia8": 100,
+    "Guia9": 100,
+    "Guia0": 100,
+    "Guia11": 100,
+}
+
+meme = ""
+memes = []
 
 informacion = {}
 
@@ -23,10 +32,14 @@ with open('informacion.json', 'r', encoding="utf_8") as f:
 
 Builder.load_file('dudas.kv')
 
+class RoundedButton(Button):
+    pass
 
-
+class Tile(RoundedButton):
+    pass
 
 class Dudas(Screen):
+
 
     def choose_info(self,title):
         print(title)
@@ -72,13 +85,63 @@ class Dudas(Screen):
             else:
                 add_item(pregunta)
 
-
-
-
     def __init__(self, **kw):
         super().__init__(**kw)
 
+        box = BoxLayout(
+            orientation="vertical",
+        )
+
+        box.add_widget(Label(
+            text="",
+            font_size = 70,
+            color=(0, 0, 0, 1),
+            size_hint=(1, 0.1),
+        ))
+
+        scroll = ScrollView()
+
+        InnerStack = StackLayout(
+            orientation='lr-tb',
+            padding= (25,10,25,10),
+            size_hint=(1, 1),
+            spacing=5,
+        )
+
+        print(len(dicGuias))
+
         self.set_list()
+
+        def meme(x):
+            global meme
+            meme = x
+            self.recor(x)
+
+        for tag in informacion.keys():
+            for x in informacion[tag].get("tags"):
+                if x not in memes:
+                    b = Tile(text=x,
+                            on_press=meme,
+                            font_size=25,
+                            size_hint=(0.3, 0.05)
+                        )
+                    memes.append(b)
+                    InnerStack.add_widget(b)
+                    
+        scroll.add_widget(InnerStack)
+
+        box.add_widget(scroll)
+
+        self.add_widget(box)
+
+    def recor(self, *args):
+        global meme
+        print(args)
+        print("Meme>",meme)
+        print(memes.index(meme))
+
+
+        self.manager.current = list(dicGuias.values())[memes.index(meme)][1]
 
     
     pass
