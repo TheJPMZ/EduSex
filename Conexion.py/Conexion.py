46d5e2c8cf327f53230b87f163bac673b7ace2bc
Estef072 <61289015+Estef072@users.noreply.github.com>
2022-06-06 16:08:28 -0600

    conexion base de datos

--- Conexion.py/Conexion.py
+++ Conexion.py/Conexion.py
@@ -2,11 +2,14 @@ from kivy.app import App
 from kivy.uix.boxlayout import BoxLayout
 from kivy.uix.button import Button
 import requests
-import json 
+import json
+import random 
+import uuid
+
 
 
 class Conexion (App):
-    firebase = "https://base-de-datos-edusex-default-rtdb.firebaseio.com/"
+    firebase = "https://base-de-datos-edusex-default-rtdb.firebaseio.com/.json"
     def build(self):
         boxLayout = BoxLayout()
         button = Button (text = 'create Patch')
@@ -15,8 +18,12 @@ class Conexion (App):
         return boxLayout
 
     def write (self , *args):
+        id= uuid.uuid1()
+        id_producto= id.hex
+        no_compa = random.randint(1,10000000002121)
+        no_compa = random.randint(1,10000)
         print("Button clicked")
-        json_data = '{"url":"nada.com","hora":"hoooy"}'
+        json_data = '{"Numero de compra": no_compa,"id_producto":id_producto,"total":}'
         dato = requests.patch(url= self.firebase,json=json.loads(json_data))
         print(dato)
 
