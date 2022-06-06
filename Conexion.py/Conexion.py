e7a15322aaecb578ae3282de8637a8a47c70b302
Estef072 <61289015+Estef072@users.noreply.github.com>
2022-06-06 12:38:51 -0600

    conexion

---
+++ Conexion.py/Conexion.py
@@ -0,0 +1,16 @@
+import random
+import requests
+import json 
+from kivy.app import App
+from kivy.uix.boxlayout import BoxLayout
+from kivy.uix.button import Button
+
+class EduSexApp(App):
+    def conectar():
+        box_layout = BoxLayout()
+        button = Button(text= "Pagar")
+        box_layout.add_widget
+
+
+if __name__ == '__main__':
+    Conexion().run()
