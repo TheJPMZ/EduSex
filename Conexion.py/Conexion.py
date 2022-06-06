7db7deed00da0d196c8654b3c7871f01f997c36b
Estef072 <61289015+Estef072@users.noreply.github.com>
2022-06-06 15:43:14 -0600

    ultimo cambio

--- Conexion.py/Conexion.py
+++ Conexion.py/Conexion.py
@@ -1,18 +1,25 @@
 from kivy.app import App
 from kivy.uix.boxlayout import BoxLayout
 from kivy.uix.button import Button
-import random
 import requests
 import json 
 
-class Conexion(App):
-    url_firebase ="https://base-de-datos-edusex-default-rtdb.firebaseio.com/"
 
-    def conectar(self):
-        boxlayout = BoxLayout()
-        button = Button(text= "Pagar")
-        boxlayout.add_widget(button)
-        return  boxlayout
+class Conexion (App):
+    firebase = "https://base-de-datos-edusex-default-rtdb.firebaseio.com/"
+    def build(self):
+        boxLayout = BoxLayout()
+        button = Button (text = 'create Patch')
+        button.bind(on_press = self.write)
+        boxLayout.add_widget(button)
+        return boxLayout
+
+    def write (self , *args):
+        print("Button clicked")
+        json_data = '{"url":"nada.com","hora":"hoooy"}'
+        dato = requests.patch(url= self.firebase,json=json.loads(json_data))
+        print(dato)
+
 
 
 if __name__ == '__main__':
