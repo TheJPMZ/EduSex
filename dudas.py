0e377fc42a610dd4255165aacab95bd437a507bf
Priscigs <fatimag811@gmail.com>
2022-06-06 09:52:15 -0600

    🔎 Fixing problems

--- dudas.py
+++ dudas.py
@@ -1,8 +1,10 @@
+from pickle import TRUE
 from kivy.properties import ListProperty
 from kivy.lang import Builder
 from kivy.uix.screenmanager import Screen
 from kivy.uix.button import Button
 from kivy.uix.boxlayout import BoxLayout
+from kivy.uix.gridlayout import GridLayout
 from kivy.uix.stacklayout import StackLayout
 from kivy.uix.label import Label
 from kivy.uix.scrollview import ScrollView
@@ -12,6 +14,7 @@ from kivymd.uix.button import MDIconButton
 import json
 
 from numpy import size
+from sympy import true
 
 dicGuias = {
     "Guia6": 100,
@@ -61,20 +64,19 @@ class Dudas(Screen):
 
         self.manager.current = "showdudas"
 
+    def choose_infoTags(self,title):
+        print(title)
+        new_dicc = {
+            "title": title,
+            "info": informacion.get(title).get("info"),
+        }
+
+        with open('innerlog.json', 'w', encoding="utf_8") as f:
+            f.write(json.dumps(new_dicc))
+
+        self.manager.current = "showdudas"
+
     def set_list(self, text="", search=False):
-
-        box = BoxLayout(
-            orientation="vertical",
-        )
-
-        box.add_widget(Label(
-            text="",
-            font_size = 100,
-            color=(0, 0, 0, 1),
-            size_hint=(1, 0.1),
-        ))
-
-        scroll = ScrollView()
 
         def add_item(name):
             self.ids.meme.add_widget(
@@ -84,7 +86,7 @@ class Dudas(Screen):
                     text=name,
                     text_size=(300, None),
                     background_normal = "",
-                    background_color= (.97, .79, .89),
+                    background_color= (.97, .73, .38),
                     color = (0,0,0),
                     on_press = lambda x: self.choose_info(name)
                 )
@@ -103,13 +105,11 @@ class Dudas(Screen):
             else:
                 add_item(pregunta)
 
-        box.add_widget(scroll)
 
-        self.add_widget(box)
-
-    def tags (self, search=False):
+    def tags (self):
         box = BoxLayout(
             orientation="vertical",
+            size_hint=(1 ,0.5)
         )
 
         box.add_widget(Label(
@@ -124,50 +124,72 @@ class Dudas(Screen):
         InnerStack = StackLayout(
             orientation='lr-tb',
             padding= (25,10,25,10),
-            size_hint=(1, 1),
+            size_hint=(1, 2),
             spacing=5,
+            pos=(6,2)
         )
 
-        print(len(dicGuias))
+        def add_itemtags(name):
+            self.ids.meme.add_widget(
+                Button(
+                    size_hint=(1, None),
+                    size_y=100,
+                    text=name,
+                    text_size=(300, None),
+                    background_normal = "",
+                    background_color= (.97, .73, .38),
+                    color = (0,0,0),
+                    # on_press = lambda x: self.choose_info(name)
+                )
+            )
 
-        def meme(x):
-            global meme
-            meme = x
-            self.recor(x)
+        self.ids.meme.clear_widgets()
 
         for tag in informacion.keys():
             for x in informacion[tag].get("tags"):
                 if not x in lista:
-                    b = Tile(text=x,
-                            on_press=meme,
+                    add_itemtags(tag)
+                    b = Button(text=x,
+                            background_color= (.97, .73, .38),
                             font_size=25,
-                            size_hint=(0.3, 0.05)
+                            size_hint=(0.3, 0.05),
+                            on_press = lambda a: self.set_list(x, True)
                         )
                     lista.append(x)
                     InnerStack.add_widget(b)
-                    
+
+        # b1 = Button(text="Fertilidad",
+        #         background_color= (.97, .73, .38),
+        #         font_size=25,
+        #         size_hint=(0.3, 0.05),
+        #         on_press = lambda a: self.set_list(text="Fertilidad")
+        #     )
+
+        # b2 = Button(text="Femenino",
+        #         background_color= (.97, .73, .38),
+        #         font_size=25,
+        #         size_hint=(0.3, 0.05),
+        #         on_press = lambda b: self.set_list(text="Feminino")
+        #     )
+
+        # b3 = Button(text="Embarazo",
+        #         background_color= (.97, .73, .38),
+        #         font_size=25,
+        #         size_hint=(0.3, 0.05),
+        #         on_press = lambda c: self.set_list(text="Embarazo")
+        #     )
+        
+        # InnerStack.add_widget(b1, b2, b3)
+             
         scroll.add_widget(InnerStack)
 
         box.add_widget(scroll)
 
         self.add_widget(box)
 
-        
-
-    def recor(self, *args):
-        global meme
-        print(args)
-        print("Meme>",meme)
-        print(memes.index(meme))
-
-
-        self.manager.current = list(dicGuias.values())[memes.index(meme)][1]
-
-
     def __init__(self, **kw):
         super().__init__(**kw)
         self.set_list()
         self.tags()
 
-    
     pass
