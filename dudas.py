2cfa2144a99b2fed2c0c2fa46cc1238604666f40
Priscigs <fatimag811@gmail.com>
2022-06-06 13:45:04 -0600

    🔎 Filtrar Again

--- dudas.py
+++ dudas.py
@@ -1,36 +1,20 @@
-from pickle import TRUE
 from kivy.properties import ListProperty
+from kivymd.app import MDApp
+from kivymd.uix.list import OneLineAvatarIconListItem
+from kivymd.uix.textfield import MDTextField
 from kivy.lang import Builder
 from kivy.uix.screenmanager import Screen
 from kivy.uix.button import Button
 from kivy.uix.boxlayout import BoxLayout
-from kivy.uix.gridlayout import GridLayout
 from kivy.uix.stacklayout import StackLayout
 from kivy.uix.label import Label
 from kivy.uix.scrollview import ScrollView
-from math import ceil
-from kivymd.uix.button import MDIconButton
+from kivy.uix.image import Image
+from kivy.uix.recycleview import RecycleView
+from kivymd.uix.expansionpanel import MDExpansionPanel, MDExpansionPanelLabel
+from kivymd.uix.boxlayout import MDBoxLayout
 
 import json
-
-from numpy import size
-from sympy import true
-
-dicGuias = {
-    "Guia6": 100,
-    "Guia7": 100,
-    "Guia8": 100,
-    "Guia9": 100,
-    "Guia0": 100,
-    "Guia11": 100,
-}
-
-meme = ""
-memes = []
-lista = [{"Tags": "Masturbacion",
-         "Tags": "Femenino",
-         "Tags": "Fertilidad"
-        }]
 
 informacion = {}
 
@@ -39,14 +23,12 @@ with open('informacion.json', 'r', encoding="utf_8") as f:
 
 Builder.load_file('dudas.kv')
 
-class RoundedButton(Button):
-    pass
-
-class Tile(RoundedButton):
-    pass
+lista = [{"Tags": "Masturbacion",
+         "Tags": "Femenino",
+         "Tags": "Fertilidad"
+        }]
 
 class Dudas(Screen):
-
 
     def choose_info(self,title):
         print(title)
@@ -64,25 +46,11 @@ class Dudas(Screen):
 
         self.manager.current = "showdudas"
 
-    def choose_infoTags(self,title):
-        print(title)
-        new_dicc = {
-            "title": title,
-            "info": informacion.get(title).get("info"),
-        }
-
-        with open('innerlog.json', 'w', encoding="utf_8") as f:
-            f.write(json.dumps(new_dicc))
-
-        self.manager.current = "showdudas"
-
     def set_list(self, text="", search=False):
-
         def add_item(name):
             self.ids.meme.add_widget(
                 Button(
                     size_hint=(1, None),
-                    size_y=100,
                     text=name,
                     text_size=(300, None),
                     background_normal = "",
@@ -105,50 +73,10 @@ class Dudas(Screen):
             else:
                 add_item(pregunta)
 
-
     def tags (self):
-        box = BoxLayout(
-            orientation="vertical",
-            size_hint=(1 ,0.5)
-        )
-
-        box.add_widget(Label(
-            text="",
-            font_size = 70,
-            color=(0, 0, 0, 1),
-            size_hint=(1, 0.1),
-        ))
-
-        scroll = ScrollView()
-
-        InnerStack = StackLayout(
-            orientation='lr-tb',
-            padding= (25,10,25,10),
-            size_hint=(1, 2),
-            spacing=5,
-            pos=(6,2)
-        )
-
-        def add_itemtags(name):
-            self.ids.meme.add_widget(
-                Button(
-                    size_hint=(1, None),
-                    size_y=100,
-                    text=name,
-                    text_size=(300, None),
-                    background_normal = "",
-                    background_color= (.97, .73, .38),
-                    color = (0,0,0),
-                    # on_press = lambda x: self.choose_info(name)
-                )
-            )
-
-        self.ids.meme.clear_widgets()
-
         for tag in informacion.keys():
             for x in informacion[tag].get("tags"):
                 if not x in lista:
-                    add_itemtags(tag)
                     b = Button(text=x,
                             background_color= (.97, .73, .38),
                             font_size=25,
@@ -156,36 +84,7 @@ class Dudas(Screen):
                             on_press = lambda a: self.set_list(x, True)
                         )
                     lista.append(x)
-                    InnerStack.add_widget(b)
-
-        # b1 = Button(text="Fertilidad",
-        #         background_color= (.97, .73, .38),
-        #         font_size=25,
-        #         size_hint=(0.3, 0.05),
-        #         on_press = lambda a: self.set_list(text="Fertilidad")
-        #     )
-
-        # b2 = Button(text="Femenino",
-        #         background_color= (.97, .73, .38),
-        #         font_size=25,
-        #         size_hint=(0.3, 0.05),
-        #         on_press = lambda b: self.set_list(text="Feminino")
-        #     )
-
-        # b3 = Button(text="Embarazo",
-        #         background_color= (.97, .73, .38),
-        #         font_size=25,
-        #         size_hint=(0.3, 0.05),
-        #         on_press = lambda c: self.set_list(text="Embarazo")
-        #     )
-        
-        # InnerStack.add_widget(b1, b2, b3)
-             
-        scroll.add_widget(InnerStack)
-
-        box.add_widget(scroll)
-
-        self.add_widget(box)
+                    self.ids.sexo.add_widget(b)
 
     def __init__(self, **kw):
         super().__init__(**kw)
